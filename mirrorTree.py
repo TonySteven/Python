5542a4260cae2ca210056690c1c764e451298051
stevenL <stevenl365404@gmail.com>
2020-11-05 16:57:24 +0800

    提交Offer27. 二叉树的镜像代码

--- mirrorTree.py
+++ mirrorTree.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# @Time    : 11/5/20 01:17
+# @Time    : 11/5/20 16:45
 # @Author  : StevenL
 # @Email   : stevenl365404@gmail.com
-# @File    : isSubStructure.py
-
+# @File    : mirrorTree.py
 
 class TreeNode(object):
     def __init__(self, x):
@@ -38,34 +37,20 @@ class Tree(object):
 
 
 class Solution:
-    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
-        def recur(A, B):
-            if not B: return True
-            if not A or A.val != B.val: return False
-            return recur(A.left, B.left) and recur(A.right, B.right)
-
-        return bool(A and B) and (
-                recur(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B))
-
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if not root: return
+        tmp = root.left
+        root.left = self.mirrorTree(root.right)
+        root.right = self.mirrorTree(tmp)
+        return root
 
 if __name__ == '__main__':
-    t1 = [3, 4, 5, 1, 2]
+    t1 = [4,2,7,1,3,6,9]
     tree1 = Tree()
-    # tree1.add(1)
-    # tree1.add(2)
-    # tree1.add(3)
     for i in t1:
-        # print("-----------", i)
         tree1.add(i)
 
-    t2 = [4, 1]
-    tree2 = Tree()
-    # tree2.add(3)
-    # tree2.add(1)
-    for i in t2:
-        # print("-----------", i)
-        tree2.add(i)
 
     s = Solution()
-    result = s.isSubStructure(tree1.root, tree2.root)
+    result = s.mirrorTree(tree1.root)
     print(result)
