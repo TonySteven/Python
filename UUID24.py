6e8c7fd701d30c81f7cad258be92ec7b246286d4
stevenL <stevenl365404@gmail.com>
2021-08-05 17:28:28 +0800

    feat: 添加UUID24.py

---
+++ UUID24.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/27 10:40
+# @Author  : StevenL
+# @Email   : stevenl365404@gmail.com
+# @File    : UUID.py
+
+import uuid
+
+
+def getuuid():
+    return "".join(str(uuid.uuid4()).split("-"))
+
+
+uuid = getuuid()
+print(uuid[0:24])
