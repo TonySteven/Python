d9e6775d7315003799891f7237db8be6d685fe68
Steven Liu <1012987522@qq.com>
2023-11-24 20:01:26 +0800

    :sparkles: feature: 更新文件.

---
+++ json/id重新赋值.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 6/10/22 16:24
+# @Author  : StevenL
+# @Email   : stevenl365404@gmail.com
+# @File    : 左右新增单引号.py
+
+
+# 加载文件
+book_path = r"/Users/steven/PycharmProjects/Python/json/book/book.txt"
+f = open(book_path, 'r', encoding='utf-8')
+file1 = open(r"/Users/steven/PycharmProjects/Python/json/book/new_book.txt", 'a')
+
+# 读取文件
+lines = f.readlines()
+
+for line in lines:
+    # line 倒数第三位数字减一
+    line = line[:-1] + str(int(line[-1]) + 1)
+
+    file1.write(line)
+    file1.write("\n")
